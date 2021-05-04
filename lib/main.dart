05d82136650ea9c3810a46c5b432f1e6948eb2ea
541844 <541844@kernhigh.org>
2021-05-04 09:25:01 -0700

    Test Commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
+//The orginal base code
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Grae',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
